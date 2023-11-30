08646519c1f2553f135e0ee6e84bb9a1046f81c6
Good Way Giver <goodwaygiver143@gmail.com>
2023-11-30 15:35:31 +0530

    2nd deploy try on cf

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,9 +59,9 @@ export default function RootLayout({
             color="#2299DD"
             initialPosition={0.08}
             crawlSpeed={200}
-            height={6}
+            height={3}
             crawl={true}
-            showSpinner={false}
+            showSpinner={true}
             easing="ease"
             speed={200}
             shadow="0 0 10px #2299DD,0 0 5px #2299DD"
