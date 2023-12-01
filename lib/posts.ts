e634fd630ba5289c6cbd64ca0e6d61282c49fc7d
Good Way Giver <goodwaygiver143@gmail.com>
2023-12-01 14:21:04 +0530

    try deploy on cf 24th

--- lib/posts.ts
+++ lib/posts.ts
@@ -5,14 +5,14 @@
 import path from "path";
 // import fs from "fs";
 
-const postsDirectory = path.join(process.cwd(), "blogposts");
+// const postsDirectory = path.join(process.cwd(), "blogposts");
 
 
 export async function getPostByName(
   fileName: string
 ): Promise<BlogPost | undefined> {
   try {
-    const fullPath = path.join(postsDirectory, fileName);
+    // const fullPath = path.join(postsDirectory, fileName);
     const content =
       '--- title: "How to install Termux?" date: "2023-11-19" tags: ["how to install termux" , "latest termux" ,"install termux" , "termux on android phone" ] image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiSq4WLc8IJaCtE_fR4IXGD4dt38lHyZr0TdLgEGSqf7oQkAtmwP_UC54CK_05roXHDzjczj602YXxSUIRfUps2bzFVXrbtzDSgniN2ELInF_cuRxwcC3QN64PhCt9rncfaDET4JYVdNRivDrckj0u61U-7Z-nFN1notDphTDPG_Fvfodg5BtYx4yztE3U/w640-h358/install_termux_post.png" link: "/how-to-install-termux" description: "Let\'s install latest termux and start coding journey on android phone ✨" --- #### If you want to install Termux on an Android phone, you have two options. 1. **Install from Play Store:** - Search for Termux and install it. 2. **Install from F-Droid Website:** - Download the Termux APK from the official F-Droid Store [here](https://f-droid.org). - Choose the version you want and click on Download APK. ![F-Droid Logo](https://f-droid.org/assets/fdroid-logo-text_S0MUfk_FsnAYL7n2MQye-34IoSNm6QM6xYjDnMqkufo=.svg) #### Step 1 » First Download APK file of Termux Download the Termux APK from the official F-Droid Store. Choose the version you want, then click on Download APK. [Download Termux APK directly 🡇](#) ![Step 1 Image](https://i0.wp.com/theankitnet.com/wp-content/uploads/2022/02/Screenshot_20220215-002947_Termux.jpg?resize=1024%2C939&ssl=1) #### Step 2 » Run Termux APK file to install Termux Once you\'ve downloaded the APK file: - Find it in the browser\'s Downloads section or in your File Manager app. - Run the file to install the Termux app. - Allow your browser or file manager to install apps from unknown sources if prompted. ![Step 2 Image](https://i0.wp.com/theankitnet.com/wp-content/uploads/2022/02/Screenshot_20220215-002947_Termux.jpg?resize=1024%2C939&ssl=1)';
     // const rawMDX = fileContents;
