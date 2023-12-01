6229570859174e44dcc8a050f17d30dacf7b1e57
Good Way Giver <goodwaygiver143@gmail.com>
2023-12-01 13:17:52 +0530

    try deploy on cf 22th

---
+++ app/api/dataapi/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { revalidatePath } from 'next/cache'
+
+export const runtime = 'edge'; // Add this line to specify the runtime
+
+export async function GET(request: NextRequest) {
+    // const secret = request.nextUrl.searchParams.get('secret')
+
+ 
+
+    // const path = request.nextUrl.searchParams.get('path') || '/'
+
+    // revalidatePath(path)
+
+    return NextResponse.json({data:""})
+}
